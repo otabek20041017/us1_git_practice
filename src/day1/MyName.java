21f14a0b7d8c676ce96c7f0757185e15520ca64f
Otabek Yusupov <news@email>
2022-08-19 15:26:29 +0500

    fgf

--- src/day1/MyName.java
+++ src/day1/MyName.java
@@ -4,5 +4,6 @@ public class MyName {
     public static void main(String[] args) {
         System.out.println("Otabek");
         System.out.println("Yusupov");
+        System.out.println("Nodirbek og'li");
     }
 }
